71029db7540f71420380a490d0da45de96ad6892
rkhominich <rkhominich21@gmail.com>
2024-10-31 21:10:18 +0800

    Добавлено задание 2

---
+++ 2024.10.27/2.py
@@ -0,0 +1,6 @@
+number = int(input("Введите целое число: "))
+
+prev = f"Перед числом {number} находится число {number-1}."
+next = f"После числа {number} идет число {number+1}."
+
+print(prev, next, sep = "\n")
