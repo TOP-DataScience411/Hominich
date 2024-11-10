4f8c22570d92ccfe568b3761667f561dbe155826
rkhominich <rkhominich21@gmail.com>
2024-11-10 17:40:50 +0800

    Задание 3

---
+++ 2024.11.03/3.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+        n = int(input("Введите натуральное число: "))
+        break
+    except ValueError:
+        continue
+
+sum_of_divisors = 0
+
+for i in range(1, int(n**0.5) + 1):
+    if n % i == 0:
+        sum_of_divisors += i
+        if i != n // i:
+            sum_of_divisors += n // i
+
+print("Сумма всех делителей числа", sum_of_divisors)
+
+# Введите натуральное число: 51
+# Сумма всех делителей числа 72
